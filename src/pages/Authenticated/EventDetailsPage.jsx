25ad4c06d2710dbde3935329f7641585df088ada
Shijuchi <shanedelatorre2412@gmail.com>
2024-03-23 14:10:40 +0800

    updated done structuring public page section

---
+++ src/pages/Authenticated/EventDetailsPage.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function EventDetailsPage(){
+    return(
+        <div></div>
+    )
+}
--- src/pages/PublicPage/EventBrowserPage.jsx
+++ src/pages/PublicPage/EventBrowserPage.jsx
@@ -1,7 +1,60 @@
 import * as React from 'react';
+import './PublicPageCss/EventBrowserPage.css'
+
 
 export default function EventBrowserPage(){
     return(
-        <div>Event Browser Page</div>
+        <div className='eventBrowser-wrapper'>
+            <div className='eventBrowser-container'>
+
+                <div className='welcome-text'>
+                    <h1>Explore Events Here!</h1>
+                </div>
+                <div className='event-secondary-text'>
+                    Explore events here, we feature new events weekly for you to explore!
+                </div>
+                <div className='eventBrowser-content'>
+                    <div className='content-header'>
+                        <h2>
+                            Featured events for this week
+                        </h2>
+                    </div>
+                    <div className='event-container'>
+                        <div className="event-item">event 1</div>
+                        <div className="event-item">event 2</div>
+                        <div className="event-item">event 3</div>
+                        <div className="event-item">event 4</div>
+                        <div className="event-item">event 5</div>
+                        <div className="event-item">event 6</div>
+                        <div className="event-item">event 7</div>
+                        <div className="event-item">event 8</div>
+                        <div className="event-item">event 9</div>
+                        <div className="event-item">event 10</div>
+                        <div className="event-item">event 11</div>
+                        <div className="event-item">event 12</div>
+                        <div className="event-item">event 13</div>
+                        <div className="event-item">event 14</div>
+                        <div className="event-item">event 15</div>
+                        <div className="event-item">event 16</div>
+                        <div className="event-item">event 17</div>
+                        <div className="event-item">event 18</div>
+                        <div className="event-item">event 19</div>
+                        <div className="event-item">event 20</div>
+                        <div className="event-item">event 21</div>
+                        <div className="event-item">event 22</div>
+                        <div className="event-item">event 23</div>
+                        <div className="event-item">event 24</div>
+                        <div className="event-item">event 25</div>
+                        <div className="event-item">event 26</div>
+                        <div className="event-item">event 27</div>
+                        <div className="event-item">event 28</div>
+                        <div className="event-item">event 29</div>
+                        <div className="event-item">event 30</div>
+                    </div>
+                </div>
+
+            </div>            
+        </div>
+        
     )
 }
