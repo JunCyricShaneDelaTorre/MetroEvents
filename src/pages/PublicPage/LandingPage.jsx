2a8002d47e92ec445796ccd90a5f7dc4c2f53c4f
Meachelle <abellameachelleabella2988@gmail.com>
2024-03-24 23:42:54 +0800

    Moved to Landing Page Content

--- src/pages/PublicPage/LandingPage.jsx
+++ src/pages/PublicPage/LandingPage.jsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './PublicPageCss/LandingPage.css';
 import Logo from './Logo.png';
-import PlantekwLogo from './PlantekwLogo.png';
-import CustomButton from './CustomButton'; 
 
 export default function LandingPage() {
-  const handleClick = () => {
-    // Handle button click event
-  };
-
+  
   return (
     <div className='LandingPage-wrapper'>
       <div className='nav-container'>
@@ -23,16 +18,8 @@ export default function LandingPage() {
           </div>
         </div>
       </div>
+      <Outlet />
 
-      <div className='container2'>
-        <Link to={'/'} className='Plantek'><img src={PlantekwLogo} alt="Plantek" /></Link>
-        <div className='title'>Teknoys Unite! Plan the Perfect Event with PlanTek.</div>
-        <div className='subtitle'>Turn Event Planning from Stress to Success</div>
-        <div className='buttonc'>
-            <CustomButton onClick={handleClick}>Explore Events</CustomButton>
-        </div>
-        
-      </div>
     </div>
   );
 }
