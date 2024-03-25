48b94354f3e4930e8b1b82937950977e3764222c
Jose Elijah M. Fernandez <fjelijah215@gmail.com>
2024-03-25 16:42:34 +0800

    Revert "User auth"

--- src/pages/PublicPage/LoginPage.jsx
+++ src/pages/PublicPage/LoginPage.jsx
@@ -1,9 +1,9 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './PublicPageCss/LoginPage.css';
-import supabase from '../../config/supabaseClient';
-import { useState } from 'react'
 
-
+export default function LoginPage() {
+    return (
         <div className='login-wrapper'>
             <div className='login-container'>
                 <div className='welcome-section'>
@@ -13,11 +13,13 @@ import { useState } from 'react'
                     </div>
                 </div>
                 <div className='login-form'>
+                    <form method='' className='form-content'>
                         <div className='email-content'>
                             <div>
                                 Email:
                             </div>
                             <div className='input-email'>
+                                <input type="email" placeholder='name@gmail.com' className='input-box' />
                             </div>
                         </div>
                         <div className='password-content'>
@@ -25,16 +27,13 @@ import { useState } from 'react'
                                 Password:
                             </div>
                             <div className="input-password">
-
+                                <input type='password' placeholder='Password' className='input-box' />
                             </div>
                         </div>
                         <button type='submit' className='submit-button'>
                             <div className='btn-text'>Login</div>
                         </button>
                     </form>
-                </div>
-                <div>
-                    
                 </div>
             </div>
         </div>
