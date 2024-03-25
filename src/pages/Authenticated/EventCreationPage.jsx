b57a3f9314313c97bac0b2759e271fe6aa2361f5
Meachelle <abellameachelleabella2988@gmail.com>
2024-03-26 06:44:31 +0800

    CreateEvent format and style added

--- src/pages/Authenticated/EventCreationPage.jsx
+++ src/pages/Authenticated/EventCreationPage.jsx
@@ -1,45 +1,57 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Form } from 'react-router-dom';
 import './AuthenticatedCss/EventCreationPage.css';
+import Logo from './Logo.png';
 
-export default function EventCreationPage(){
-    return(
+export default function EventCreationPage() {
+    const [description, setDescription] = useState('');
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent form submission
+            setDescription(description + '\n'); // Append newline character
+        }
+    };
+
+    return (
         <div className='event-creation-wrapper'>
             <div className='event-creation-container'>
-
                 <div className='event-creation-image-container'>
-                    IMAGE
+                    <img src={Logo} alt="" /> 
                 </div>
-                    <div className='event-creation-details-container'>
-                        <Form method='post'>
-                            <div className='event-form-event-name'>
-                                <input type="text" placeholder='Event Name'/>
-                            </div>
-                            <div className='event-form-event-start'>
-                                <label>
-                                    <span>Start</span>
-                                    <input type='datetime-local' placeholder='Start'/>
-                                </label>
-                            </div>
-                            <div className='event-form-event-end'>
-                                <label>
-                                    <span>End</span>
-                                    <input type="datetime-local" placeholder='End'/>
-                                </label>
-                            </div>
-                            <div className=''>
-                        
-                            </div>
-                            <div></div>               
-                            
-                            <input type="text" name="" id="" placeholder='Add Description' />
-                        
+                <div className='title1'>
+                    Creating your event
+                </div>
+                <div className='event-creation-details-container'>
+                    <Form method='post'>
+                        <div className='event-form-event-name'>
+                            <input type="text" placeholder='Event Name' />
+                        </div>
+                        <div>
+                            Start Date:
+                        </div>
+                        <div className='event-form-event-start'>
+                            <input type='datetime-local' placeholder='Start' />
+                        </div>
+                        <div>
+                            End Date:
+                        </div>
+                        <div className='event-form-event-end'>
+                            <input type="datetime-local" placeholder='End' />
+                        </div>
+                        <div className='event-form-event-description'>
+                            <textarea 
+                                placeholder='Add Description' 
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                            />
+                        </div>
                         <button type='submit'>Create Event</button>
-                        </Form>
-                    </div>
-            
-           
+                    </Form>
+                </div>
             </div>
         </div>
-    )
+    );
 }
