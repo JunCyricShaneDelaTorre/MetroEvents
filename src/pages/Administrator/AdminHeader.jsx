64c10b089a809335f20e86b38d8118b23e0c0ec3
Meachelle <abellameachelleabella2988@gmail.com>
2024-03-26 18:37:50 +0800

    admin header

--- src/pages/Administrator/AdminHeader.jsx
+++ src/pages/Administrator/AdminHeader.jsx
@@ -1,34 +1,55 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import './AdminCss/AdminHeader.css';
+import Logo from './Logo.png';
+import Events from './Events.png';
+import Create from './Create.png';
+import Explore from './Explore.png';
+import Notification from './Notification.png';
+import Signout from './Signout.png';
 
 export default function AdminHeader(){
     return(
         <div className='nav-wrapper'>
-                <div className='nav-container'>
-                    <nav className='admin-nav-bar'>
-                        <div className='home-logo'>
-                            <Link to={''} className='admin-nav-links'>Admin</Link>
-                        </div>
-                        <div className='admin-center-and-right'>
-                            <div className='admin-center-container'>
-                                <div className='dashboard-home-btn'>
-                                    <Link to={''} className='admin-nav-links'>Organizer</Link>
-                                </div>
-                                <div className='create-event-btn'>
-                                    <Link to={''} className='admin-nav-links'>Users</Link>    
-                                </div>
-                                <div>
-                                    <Link to={''} className='admin-nav-links'>Events</Link>
-                                </div>
+            <div className='nav-container'> 
+                <nav className='navbar'>
+                        <Link to={'/home'} className='imgl'><img src={Logo} alt="Logo" /></Link>
+                    <div className='center-and-right'>
+                        <div className='center-container'>
+                            <div className='nav'>
+                                <Link to={'/home'} className='Events'><img src={Events} alt="" /></Link>
                             </div>
-                            <div className='admin-right-container'>
-                                <div>notif</div>
-                                <div><Link to={''} className='admin-nav-links'>Logout</Link></div>
+
+                            <div className='nav'>
+                                <Link to={'OrganizeEvents'} className='Create'><img src={Create} alt="" /></Link>
                             </div>
+
+                            <div className='nav'>
+                                <Link to={'BrowseEvents'} className='Explore'><img src={Explore} alt="" /></Link>
+                            </div>
+
+{/* 
+                            <Link to={'OrganizeEvents'} className='nav-links'>Create Event</Link>
+                            <Link to={'BrowseEvents'} className='nav-links'>Explore Events</Link> */}
                         </div>
-                    </nav>
-                </div>
+
+
+
+                        <div className='right-container'>
+                            <div className='nav'>
+                                <Link to={'Notifications'} className='Notification'><img src={Notification} alt="" /></Link>
+                            </div>
+                            <div className='nav'>
+                                <Link to={'/'} className='Signout'><img src={Signout} alt="" /></Link>
+                            </div>
+                            
+                            
+                            {/* <div>notif</div> */}
+                            {/* <Link to={'Profile'} className='nav-links'>Profile</Link> */}
+                        </div>
+                    </div>
+                </nav>
             </div>
-    )
+        </div>
+    );
 }
